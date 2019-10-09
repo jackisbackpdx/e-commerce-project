5f3e10903f198c2e2ece2e28d7ce911cded2ac73
Jackson Kelley <jackson.kelley.g@gmail.com>
2019-10-09 16:25:43 -0700

    removed unnecessary console logs

--- shopping-cart/render-line-item.js
+++ shopping-cart/render-line-item.js
@@ -4,12 +4,9 @@ import { instruments } from '../products/app.js';
 import { cart } from '../products/app.js';
 
 const tBody = document.querySelector('tbody');
-console.log(instruments);
 for (let i = 0; i < cart.length; i++) {
     const lineItem = cart[i];
-console.log(lineItem.code, 'code');
     const chosenInstrument = findProducts(instruments, lineItem.code);
-    console.log(chosenInstrument);
     const dom = makeTr(chosenInstrument, lineItem);
     
     tBody.appendChild(dom);
